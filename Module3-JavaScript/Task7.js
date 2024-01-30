125c7b05071567f5e68d8f99f2229235a7de7c97
Lazbat <babayalelazbat@gmail.com>
2024-01-30 23:20:29 +0100

    add new files

---
+++ Module3-JavaScript/Task7.js
@@ -0,0 +1,35 @@
+const side1 = 60
+const side2 = 60
+const side3 = 60
+
+if (side1===side2 && side2===side3) {
+    console.log('Equilateral Triangle')
+} else if(side1===side2 || side1===side3 || side2===side3) {
+    console.log('Isosceles Triangle')
+} else {
+    console.log('Scalene Triangle')
+}
+
+const side4 = 55
+const side5 = 55
+const side6 = 70
+
+if (side4===side5 && side5===side6) {
+    console.log('Equilateral Triangle')
+} else if(side4===side5 || side4===side6 || side5===side6) {
+    console.log('Isosceles Triangle')
+} else {
+    console.log('Scalene Triangle')
+}
+
+const side7 = 50
+const side8 = 60
+const side9 = 70
+
+if (side7===side8 && side9===side6) {
+    console.log('Equilateral Triangle')
+} else if(side7===side8 || side7===side9 || side8===side9) {
+    console.log('Isosceles Triangle')
+} else {
+    console.log('Scalene Triangle')
+}
