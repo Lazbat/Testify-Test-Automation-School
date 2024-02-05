6699756b554374a818b5cc65d4e604594596fff5
Lazbat <babayalelazbat@gmail.com>
2024-02-05 22:15:17 +0100

    new file

---
+++ Module3-JavaScript/Task13.js
@@ -0,0 +1,11 @@
+const books = {
+    title:'True Betrayal',
+    description:'Romance',
+    numberOfPages:345,
+    author:'Nora Roberts',
+    reading:true
+}
+
+console.log(books.description)
+console.log(books['author'])
+
