3a91fbe32de1f657b5b8e2d4da333b991fe7159d
Lazbat <babayalelazbat@gmail.com>
2024-02-14 20:51:01 +0100

    add new file

---
+++ Module3-JavaScript/JS-CodingChallenge/JS-CodingChallenge6.js
@@ -0,0 +1,4 @@
+let programmingLanguages = ['JavaScript', 'Python', 'Java', 'C++', 'C#', 'PHP', 'Ruby'];
+//the sort() method is used to sort an array alphabetically in JavaScript
+programmingLanguages.sort();
+console.log(programmingLanguages);
