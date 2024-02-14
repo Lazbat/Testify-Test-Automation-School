76ba2765a48df995d2081a78270fd204a8535a7e
Lazbat <babayalelazbat@gmail.com>
2024-02-14 21:41:39 +0100

    new commit

---
+++ Module3-JavaScript/JS-CodingChallenge/JS-CodingChallenge8.js
@@ -0,0 +1,13 @@
+const number = 60
+if (number%10===0) {
+    console.log(true)
+} else {
+   console.log(false) 
+}
+
+const number1 = 87
+if (number1%10===0) {
+    console.log(true)
+} else {
+   console.log(false)
+}
