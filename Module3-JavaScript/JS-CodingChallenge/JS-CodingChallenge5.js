18a48c401d69510236960b6f54c0878acac68ba1
Lazbat <babayalelazbat@gmail.com>
2024-02-15 20:51:53 +0100

    commit new file

---
+++ Module3-JavaScript/JS-CodingChallenge/JS-CodingChallenge5.js
@@ -0,0 +1,14 @@
+//the 'reverse()' function is used to reverse elements in an array
+function reverseArray(array) {
+    return array.reverse();
+}
+const myArray = [14,78,9,0,56,8];
+const reversedArray = reverseArray(myArray);
+console.log("Reversed Array:", reversedArray)
+
+function reverseArray1(array) {
+    return array.reverse();
+}
+const myArray1 = ['Python', 'Java', 'JavaScript']
+const reversedArray1 = reverseArray1(myArray1);
+console.log("Reversed Array1:", reversedArray1)
