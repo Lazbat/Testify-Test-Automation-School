704b66b573091de151fa2de99f9596faea1db6db
Lazbat <babayalelazbat@gmail.com>
2024-03-18 23:28:32 +0100

    new files

---
+++ Module4-Java/Module4A/Tasks/Task11/ClassA.java
@@ -0,0 +1,11 @@
+package Task11;
+
+public class ClassA {
+    static String name;
+
+    public static void aboutMe(String myName){
+        name = myName;
+        System.out.println(name);
+    }
+
+}
---
+++ Module4-Java/Module4A/Tasks/Task11/ClassB.java
@@ -0,0 +1,11 @@
+package Task11;
+
+public class ClassB {
+    public static void main(String[] args) {
+        ClassA object = new ClassA();
+        ClassA.name = "Lazbat";
+
+        ClassA.aboutMe(ClassA.name);
+
+    }
+}
