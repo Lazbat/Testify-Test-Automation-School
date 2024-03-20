d75a7200f0641e5bd26f4124e01d6117720eb06c
Lazbat <babayalelazbat@gmail.com>
2024-03-20 23:13:37 +0100

    new file

---
+++ Module4-Java/Module4A/Tasks/Task16A.java
@@ -0,0 +1,30 @@
+package org.Tasks;
+
+public class Task16A {
+    //OVERLOADING:Create a method in a class and Overload in using the 3 different ways of overloading that you know.
+    public void readBooks(){
+
+    }
+    //Way 1: Number of parameters
+    public void readBooks(int number){
+
+    }
+    public void readBooks(int number, int numbers){
+
+    }
+    //Way 2: Data type of parameters
+    public void readBooks(int number, float data){
+
+    }
+    public void readBooks(String name, byte value){
+
+    }
+    //Way 3: Sequence of data type of parameters
+    public void readBooks(boolean condition, float data){
+
+    }
+    public void readBooks(float data, boolean condition){
+
+    }
+
+}
