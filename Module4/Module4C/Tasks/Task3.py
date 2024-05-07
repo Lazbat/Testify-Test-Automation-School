b457d3bd7d19031ae6b6b03c525a6967db87efd5
Lazbat <babayalelazbat@gmail.com>
2024-05-08 00:54:52 +0100

    Task3

---
+++ Module4/Module4C/Tasks/Task3.py
@@ -0,0 +1,19 @@
+# Declare a raw string
+# Declare two strings with any value
+# Concatenate the two string
+# Print out the value of the concatenated strings
+
+language = "I am on my third programming language\npython is the language\n\tit seems quite simple!\b"
+# raw string
+language = r"I am on my third programming language\npython is the language\n\tit seems quite simple!\b"
+print(language)
+
+# declare two strings with any value
+string_one = "Foot"
+string_two = 'ball'
+
+# Concatenate the two string
+sport = string_one + string_two
+
+# Print out the value of the concatenated strings
+print(sport)
