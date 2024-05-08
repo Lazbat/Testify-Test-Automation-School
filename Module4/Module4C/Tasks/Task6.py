ee890d7ab27c7d5aa0212df945a74eb9a5043437
Lazbat <babayalelazbat@gmail.com>
2024-05-08 23:34:16 +0100

    Task6

---
+++ Module4/Module4C/Tasks/Task6.py
@@ -0,0 +1,33 @@
+# Declare two numbers with any values
+# Check if the first number is greater than 50 and check if the second number is less than 100,
+# if True print out "Evaluation is True"
+# Add an else block that prints "Evaluation is False"
+# Declare a string with value "python"
+# Check if the first string is equal to "testify" or "python",
+# if True prints out "Yay!! The string is a Testify Python".
+
+# Use the logical and to check if the first number is greater than 50 and the second number is less than 100.
+
+# Declare two numbers with any values
+number1 = 65
+number2 = 80
+
+# Check if the first number is greater than 50 and check if the second number is less than 100,
+# if True print out "Evaluation is True"
+print("Logical And:\n")
+if number1 > 50 and number2 < 100:
+    print("Evaluation is True")
+    # Add an else block that prints "Evaluation is False"
+else:
+    print("Evaluation is False")
+
+print("\nLogical Or:\n")
+# Declare a string with value "python"
+string = "python"
+if string == "testify" or string == "python":
+    # if True prints out "Yay!! The string is a Testify Python".
+    print("Yay!! The string is a Testify Python")
+
+
+
+
