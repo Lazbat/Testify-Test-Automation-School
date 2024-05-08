caffea3bfe7d7e1cba83ede6695b70cb98040aed
Lazbat <babayalelazbat@gmail.com>
2024-05-08 01:18:51 +0100

    Task4

---
+++ Module4/Module4C/Tasks/Task4.py
@@ -0,0 +1,15 @@
+# Declare two numbers with any values
+# Add or Subtract or Multiply the two numbers together
+# Print out the value of the operation
+
+number_one = 10
+number_two = 2
+
+addition = number_one + number_two
+print(addition)
+
+subtraction = number_one - number_two
+print(subtraction)
+
+multiplication = number_one * number_two
+print(multiplication)
