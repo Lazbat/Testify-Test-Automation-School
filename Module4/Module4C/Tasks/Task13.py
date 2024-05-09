bd6284b01ac1c9907204d98e16d7bc5d8acc2f82
Lazbat <babayalelazbat@gmail.com>
2024-05-09 21:14:32 +0100

    Task13

---
+++ Module4/Module4C/Tasks/Task13.py
@@ -0,0 +1,13 @@
+# Create a function that prints out "Hello World"
+# Invoke the same function in it own body
+# Invoke the function outside the function block
+
+# Take note of the function invoke and put the Python whitespace rule in mind
+
+
+def greet():
+    print("Hello World")
+    greet()
+
+
+greet()
