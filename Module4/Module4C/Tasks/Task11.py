0cd89644b4ce60450ce7e7b8ef7f6c3b8503a62c
Lazbat <babayalelazbat@gmail.com>
2024-05-09 17:36:06 +0100

    Task11

---
+++ Module4/Module4C/Tasks/Task11.py
@@ -0,0 +1,15 @@
+# Create a function that accepts two numbers, adds the numbers and prints out the result in the console.
+# Create a function that return the string value "Testify Python"
+# Invoke/call the two functions
+
+def add(num1, num2):
+    result = num1 + num2
+    print("Addition of ", num1, "+", num2, "=", result)
+
+
+def string():
+    return "Testify Python"
+
+
+add(14, 7)
+print(string())
