bb2da905a4e212008a061d427655f97df8502a5a
Lazbat <babayalelazbat@gmail.com>
2024-05-09 01:07:17 +0100

    Task9

---
+++ Module4/Module4C/Tasks/Task9.py
@@ -0,0 +1,15 @@
+# Create a function that prints out "Hello World"
+# Create a function that does nothing, an empty function
+# Invoke/call both functions
+
+def greet():
+    print("Hello World")
+    print("My name is Lazbat")
+
+
+def your_address():
+    pass
+
+
+greet()
+your_address()
