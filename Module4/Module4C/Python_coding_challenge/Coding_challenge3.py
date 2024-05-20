3dfc347cecf64839d3fbd96abbf111f355eac912
Lazbat <babayalelazbat@gmail.com>
2024-05-20 21:39:13 +0100

    coding challenge

---
+++ Module4/Module4C/Python_coding_challenge/Coding_challenge3.py
@@ -0,0 +1,6 @@
+# Print all even numbers from 0-100.
+
+# range(0, 101) includes 0 and 100
+for number in range(0, 101):  
+    if number % 2 == 0:  # Check if the number is even
+        print(number)
