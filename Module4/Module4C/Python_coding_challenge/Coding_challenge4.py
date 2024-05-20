94d1f7141f1c4fdebc77d0b9ec6220fa814f50a0
Lazbat <babayalelazbat@gmail.com>
2024-05-20 21:52:05 +0100

    coding challenge

---
+++ Module4/Module4C/Python_coding_challenge/Coding_challenge4.py
@@ -0,0 +1,8 @@
+# Print a table containing multiplication tables
+
+# Multiplication table (from 1 to 12)
+num = 12
+
+# Iterate 12 times from i = 1 to 10
+for i in range(1, 13):
+   print(num, 'x', i, '=', num*i)
