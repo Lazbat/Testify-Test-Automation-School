3e26ca9406ae63f5608f9e11a63fb253d8a162f4
Lazbat <babayalelazbat@gmail.com>
2024-05-20 20:38:35 +0100

    coding challenge

---
+++ Module4/Module4C/Python_coding_challenge/Coding_challenge1.py
@@ -0,0 +1,10 @@
+# Calculate the sum of numbers within an array.
+
+# Array of numbers
+array_of_numbers = [13, 24, 35, 46, 57]
+
+# Using the sum() function to calculate the sum of numbers in an array
+total = sum(array_of_numbers)
+
+# Print the result
+print("The sum of the numbers in the array is:", total)
